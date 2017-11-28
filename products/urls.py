ed183d9ab5ace79b0ef7a9cb774ce50cd6175467
Peter Kosinski <peter.kosinski.irl@gmail.com>
2017-11-28 16:18:21 +0000

    intial commit

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from .views import all_products, do_search
+
+urlpatterns = [
+    url(r'^$', all_products, name="all_products"),
+    url(r'^search/', do_search, name="search"),
+    
+ ]
